cd8a10e50abb6de2e4582615984e8b0e60df4e69
Sophy <snasambu40@gmail.com>
2025-08-25 16:11:38 +0300

    Documented CRUD operations for the bookshelf app

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,8 +1,10 @@
+# LibraryProject/bookshelf/models.py
+
 from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
 
     def __str__(self):
