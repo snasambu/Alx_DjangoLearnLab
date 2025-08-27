cf47f8dbce8612adb88347c2f4342eea982ffa48
Sophy <snasambu40@gmail.com>
2025-08-27 16:35:28 +0300

    Add Book model and CRUD markdown files

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
