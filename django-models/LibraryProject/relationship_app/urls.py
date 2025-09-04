0b83a57be6d1ef2672bc095705f7e4182c04c597
Sophy <snasambu40@gmail.com>
2025-09-04 21:38:28 +0300

    Append add_book and edit_book URLs without altering previous code

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -83,3 +83,7 @@ urlpatterns += [
     path('books/add/', add_book, name='add_book'),
     path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
 ]
+urlpatterns += [
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+]
