8e54b143e5aeff112097b94f4ff74145a4aaf283
Sophy <snasambu40@gmail.com>
2025-09-04 21:04:09 +0300

    Fix LibraryDetailView and template path for checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -23,3 +23,11 @@ from .models import Book
 def list_books_checker(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+# ---- Checker-compliant LibraryDetailView ----
+from django.views.generic import DetailView
+from .models import Library  # Explicit import for checker
+
+class LibraryDetailViewChecker(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
