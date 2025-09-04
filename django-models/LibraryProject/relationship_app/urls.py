b5f9dfaef30f5f6e78f0ffb3ad785baf50fd6c33
Sophy <snasambu40@gmail.com>
2025-09-04 21:30:09 +0300

    Add custom permissions and secured views for Book model

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -33,3 +33,10 @@ from . import views
 urlpatterns += [
     path("register/", views.register_view, name="register"),
 ]
+from .views import add_book, edit_book, delete_book
+
+urlpatterns += [
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -88,3 +88,22 @@ def member_required(user):
 @user_passes_test(member_required)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+
+# ---- Secured Views ----
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
