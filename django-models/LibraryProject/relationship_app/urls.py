776b8a4af932ac05a55107f33a2f93377056fd3f
Sophy <snasambu40@gmail.com>
2025-09-04 21:32:34 +0300

    Add URLs for add_book and edit_book views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -40,3 +40,10 @@ urlpatterns += [
     path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
 ]
+# ---- Secured Book Views URLs ----
+from .views import add_book, edit_book
+
+urlpatterns += [
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
+]
