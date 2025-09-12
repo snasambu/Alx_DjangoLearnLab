cc52294e2e58ec8da4511b9b9ad3e6de9c9495c2
Sophy <snasambu40@gmail.com>
2025-09-12 21:49:23 +0300

    Registered CustomUser in admin with extra fields

--- advanced_features_and_security/accounts/models.py
+++ advanced_features_and_security/accounts/models.py
@@ -26,7 +26,5 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
 
-    objects = CustomUserManager()
-
     def __str__(self):
         return self.username
