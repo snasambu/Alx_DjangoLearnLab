35574391bdd36b95fba1664560595c0b9ccdc0e6
Sophy <snasambu40@gmail.com>
2025-09-14 10:37:57 +0300

    Fix bookshelf app path and finalize CRUD docs

--- LibraryProject/LibraryProject/bookshelf/apps.py
+++ LibraryProject/LibraryProject/bookshelf/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookshelf'
+    name = 'LibraryProject.bookshelf'
