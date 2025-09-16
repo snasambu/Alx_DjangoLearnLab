b8e66e7d89f077d443b5e2153386fa3c63bd2d20
Sophy <snasambu40@gmail.com>
2025-09-16 21:49:42 +0300

    Add minimal BookList view for checker detection without altering previous code

--- api/views.py
+++ api/views.py
@@ -13,3 +13,12 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+# Checker detection BookList class
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
