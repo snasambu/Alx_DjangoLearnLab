139f6547e98f96b3cb7ab473afe88307c350de54
Sophy <snasambu40@gmail.com>
2025-09-16 21:45:00 +0300

    Include api app urls in project-level urls.py for BookList endpoint

--- api_project/urls.py
+++ api_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # make sure 'include' is imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls')),  # <-- include your api app's urls
 ]
