d24bd9e7ffa460f01b2ff61497fb07060265f222
Sophy <snasambu40@gmail.com>
2025-09-29 22:53:23 +0300

    Resolve merge conflict in settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,3 +122,6 @@ STATICFILES_DIRS = [ BASE_DIR / "static" ]   # optional global static files dir
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
+
